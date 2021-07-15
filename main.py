265780ea7edde941bd7d1f837452549abb7d2e7b
Pavlos Constas <pconstasm@gmail.com>
2021-07-15 19:29:16 -0400

    added function to make code cleaner

--- main.py
+++ main.py
@@ -1,34 +1,32 @@
 import pygame
 import random
 
+letters = ["1", "2", "3", "4", "5", "6", "7",
+           "8", "9", "0", "a", "b", "c", "d", "e", "f"]
+
+
 def main():
-
-    pygame.font.init()
-    letters = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "a", "b", "c", "d", "e", "f"]
-    newColor = False
+    surf = pygame.image.load("bitmap.png")
+    pygame.display.set_icon(surf)
     
-    colorcode = "#"
-    for i in range(0, 6):
-        rand = random.randint(0, 15)
-        colorcode += letters[rand]
+    newColor = False
+    colorcode = genColor(letters)
     bgcolor = hex_to_rgb(colorcode)
+    
 
-    font_obj = pygame.font.SysFont("comicsansms", 45)
-
-    (width, height) = (300, 500)
-    screen = pygame.display.set_mode((width, height))
+    screen = pygame.display.set_mode((300, 500))
     pygame.display.set_caption('Random Color Generator')
     screen.fill(bgcolor)
 
-    text_obj=font_obj.render(colorcode,True,(255, 255, 255))
-    screen.blit(text_obj, (22,0))
+    pygame.font.init()
+    font_obj = pygame.font.SysFont("comicsansms", 45)
+    text_obj = font_obj.render(colorcode, True, (255, 255, 255))
+    screen.blit(text_obj, (22, 0))
 
     pygame.display.flip()
 
-    surf = pygame.image.load("bitmap.png")
-    pygame.display.set_icon(surf)
+    
     running = True
-
 
     while running:
         for event in pygame.event.get():
@@ -41,25 +39,30 @@ def main():
                     quit()
                 if event.key == pygame.K_RETURN:
                     newColor = True
-                
+
         while(newColor):
-            colorcode = "#"
-            for i in range(0, 6):
-                    rand = random.randint(0, 15)
-                    colorcode += letters[rand]
+            colorcode = genColor(letters)
             bgcolor = hex_to_rgb(colorcode)
             print(bgcolor)
             screen.fill(bgcolor)
-            text_obj=font_obj.render(colorcode,True,(255, 255, 255))
-            screen.blit(text_obj, (22,0))
+            text_obj = font_obj.render(colorcode, True, (255, 255, 255))
+            screen.blit(text_obj, (22, 0))
             pygame.display.update()
             newColor = False
+
+
+def genColor(letters):
+    colorcode = "#"
+    for i in range(0, 6):
+        rand = random.randint(0, 15)
+        colorcode += letters[rand]
+    return colorcode
 
 
 def hex_to_rgb(value):
     value = value.lstrip('#')
     lengthVal = len(value)
-    return tuple(int(value[i:i + lengthVal // 3], 16) for i in range(0, lengthVal,lengthVal // 3))
+    return tuple(int(value[i:i + lengthVal // 3], 16) for i in range(0, lengthVal, lengthVal // 3))
 
 
 main()
